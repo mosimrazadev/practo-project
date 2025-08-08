e2ad892c13dff5222b213a74d961f03e38a01a5f
mosimrazadev <mosimraza.webdev@gmail.com>
2025-08-09 05:19:22 +0530

    final

--- server/index.js
+++ server/index.js
@@ -3,16 +3,17 @@ const express = require("express");
 const connectDB = require("./src/config/db");
 const citiesRoutes = require("./src/routes/indianCities.routes")
 const cors = require("cors");
+const path = require('path');
 
 
 
 const app = express();
-const port = 5000
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 // Middlewares
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // allow frontend
@@ -30,6 +31,9 @@ app.get("/", (req, res) => {
 
 app.use("/cities", citiesRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 
 app.listen(port,  () => {
