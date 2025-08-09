8b7c2d77050f31129b56b6ff29a9dd5ee6e89f7c
mosimrazadev <mosimraza.webdev@gmail.com>
2025-08-09 05:47:31 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 
 app.use("/cities", citiesRoutes);
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
